24b0bc0b0acfda80c7197d98ceecb002da311f40
Youcef Kacer <youcef.kacer@gmail.com>
2020-07-20 15:19:52 +0200

    Update cocoeval.py

--- pycocotools/pycocotools/cocoeval.py
+++ pycocotools/pycocotools/cocoeval.py
@@ -497,7 +497,7 @@ class COCOeval:
 
         def _summarizeDets():
             stats = np.zeros((12, ))
-            stats[0] = _summarize(1)
+            stats[0] = _summarize(1, maxDets=self.params.maxDets[2])
             stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
             stats[2] = _summarize(1,
                                   iouThr=.75,
